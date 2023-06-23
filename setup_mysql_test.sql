c64a74e25731f90e427fd9e3262c5f4e3d5d0cca
BkayOne <chiedoziefrankelly@gmail.com>
2023-06-23 13:13:37 -0700

    Hot fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,5 @@
--- this script prepares a MySQL server for the project
-
--- create project test database with the name: hbnb_test_db
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
--- creating new user named: hbnb_test with all privileges on the db hbnb_test_db
--- with the password: hbnb_test_pwd if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
--- granting all privileges to the new user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
-
--- granting the SELECT privilege for the user hbnb_test in the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
